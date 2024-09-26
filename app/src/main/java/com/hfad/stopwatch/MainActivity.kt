e1357025573c03da244ced7f39246c501a3e3a59
wnkdsmm <lwjfkg@mail.ru>
2024-09-26 13:05:27 +0700

    Stopwatch-commit1

---
+++ app/src/main/java/com/hfad/stopwatch/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.hfad.stopwatch
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.hfad.stopwatch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+
+}
